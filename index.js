f0635e51a74adf75a4d58a1ab8bbc39d78b64cab
giang <TG@PC01.local>
2016-06-30 07:46:41 +0900

    [JSON-1] Parse simple 1

--- index.js
+++ index.js
@@ -2,7 +2,17 @@
  * Created by TG on 6/29/16.
  */
 $(document).ready(function(){
-    $("button").click(function(){
-        $(this).hide();
+    var textArea = $("#jsonText");
+    var parseArea = $("#jsonParse");
+    var parseContent = null;
+    var textContent = null;
+    $("button").click(function () {
+        textContent = $.trim(textArea.val());
+
+        var obj = $.parseJSON(textContent);
+        function decode() {
+            console.log(obj);
+        }
+        decode();
     });
 });
